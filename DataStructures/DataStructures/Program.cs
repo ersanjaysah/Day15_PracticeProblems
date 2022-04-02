2e824188a73234ea397b321d373ec52d3b6339a0
sanjay <ssah6694@gmail.com>
2022-04-02 08:51:56 +0530

    [sanjay]Added: frequency file

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -6,7 +6,18 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The Data structures Programs");
-        }
+            Console.WriteLine("Hello, welcome to HashTable Problem");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value is: " + hash5);
+        }   
     }
 }
